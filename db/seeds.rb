09c41396309d404ceb8ea73b0dc0d902143d1130
Andras Varga <vargan89@gmail.com>
2022-12-06 22:54:34 +0800

    seeding database done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+require 'open-uri'
+
+page_number = 0
+
+loop do
+  page_number += 1
+  url = "http://tmdb.lewagon.com/movie/top_rated?&page=#{page_number}"
+  movies = URI.open(url).read
+  movie = JSON.parse(movies)
+  movie_results = movie['results']
+  movie_poster = 'https://image.tmdb.org/t/p/w500'
+
+  movie_results.each do |m|
+    new_movie = Movie.create(title: m['title'], overview: m['overview'], poster_url: movie_poster + m['poster_path'], rating: m['vote_average'])
+    puts "#{new_movie.title} has been created!"
+  end
+
+  if page_number == 4
+    break
+  end
+end
+
+puts Movie.count.to_s
